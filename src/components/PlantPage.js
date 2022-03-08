358e74988d2e6d87f0256ce7d3e822566868c5fa
mon33k <moniquemojica@pursuit.org>
2022-03-07 19:16:52 -0500

    added to imagecontext

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,36 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLocation } from "react-router-dom";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "../stylesheets/PlantPage.css";
+import { ImageContext } from "../provider/imageContext";
 
 const PlantPage = () => {
+  const plantImagePaths = useContext(ImageContext);
+
   const location = useLocation();
   const { from } = location.state;
-  const imgList = Array.from(Array(from["# pics"] + 1).keys()).slice(1);
-  console.log(from);
+  const plantPathArr = plantImagePaths.getPlantImgs(from);
 
-  const getSpecificImgPath = imgList.map((num) => {
-    // Some file paths have a space, or begins with a lowercase instead of capitalized
-    let filePath = require(`../api/dataset/${from.Name}/${
-      num !== 1 ? from.Name + num + ".jpg" : from.Name + ".jpg"
-    }`);
-
-    if (from.Name)
-      return (
-        <Image
-          rounded="true"
-          src={filePath}
-          key={Math.random(num)}
-          fluid="true"
-        />
-      );
-  });
+  console.log("plantPathArr", plantPathArr);
 
   return (
     <>
       <Container fluid="md">
         <Row>
-          <Col className="img-container">{getSpecificImgPath}</Col>
+          <Col className="img-container">
+            {plantPathArr.map((plant, i) => plant)}
+          </Col>
           <Col>
             <h2>{from.Name}</h2>
             <h4>{from["Scientific name"]}</h4>
--- src/provider/imageContext.js
+++ src/provider/imageContext.js
@@ -1,10 +1,34 @@
 import { createContext } from "react";
+import { Image } from "react-bootstrap";
 
 export const ImageContext = createContext();
 
 export const ImageProvider = ({ children }) => {
+  const getPlantImgArr = (plant) => {
+    const imgList = Array.from(Array(plant["# pics"] + 1).keys()).slice(1);
+
+    const imgPathList = imgList.map((num) => {
+      // Some file paths have a space, or begins with a lowercase instead of capitalized
+      let filePath = require(`../api/dataset/${plant.Name}/${
+        num !== 1 ? plant.Name + num + ".jpg" : plant.Name + ".jpg"
+      }`);
+      return filePath;
+    });
+
+    return imgPathList.map((eachPlant, i) => {
+      return (
+        <Image
+          rounded="true"
+          src={eachPlant}
+          key={Math.random(i)}
+          fluid="true"
+        />
+      );
+    });
+  };
+
   return (
-    <ImageContext.Provider value={"image path array here"}>
+    <ImageContext.Provider value={{ getPlantImgs: getPlantImgArr }}>
       {children}
     </ImageContext.Provider>
   );
